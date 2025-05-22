2c1ed24f02fee68201b392aa3e4e1fbf250caae3
Suganth Manoharan <romansuganth1762001@gmail.com>
2025-05-22 10:57:30 +0530

    edit server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use('/api/reminders', reminderRoutes);
 
 connectDB()
 
-const PORT = process.env.PORT || 5000;
+const PORT = import.meta.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
