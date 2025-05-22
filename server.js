12417664a07346475e20f206bfe81f0d046698d7
Suganth Manoharan <romansuganth1762001@gmail.com>
2025-05-22 11:35:55 +0530

    add cors in server.js

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use('/api/reminders', reminderRoutes);
 
 connectDB()
 
-const PORT = import.meta.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
